da2b5676bc60d7e0e2dd5bd36e6e8d6c136f8262
Braeden Wright <braeden.wright21@gmail.com>
2022-01-18 17:01:03 -0600

    main

---
+++ data_types_and_variables.py
@@ -0,0 +1,21 @@
+# Movie Rentals
+
+little_mermaid = 3
+brother_bear = 5
+hercules = 1
+
+cost_per_day = 3
+
+cost_of_rentals = (little_mermaid + brother_bear + hercules) * cost_per_day
+
+
+#  Contractor
+
+employers = [
+    {'company': 'Google', 'pay_rate': 400, 'hrs_this_week': 6}, 
+    {'company': 'Amazon', 'pay_rate': 380, 'hrs_this_week': 4}, 
+    {'company': 'Facebook', 'pay_rate': 350, 'hrs_this_week': 10}
+]
+
+
+
