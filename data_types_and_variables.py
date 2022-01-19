e41ff929342c5d15cb0aae49ac3f0136bc072c67
Braeden Wright <braeden.wright21@gmail.com>
2022-01-19 09:53:10 -0600

    main

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -9,13 +9,37 @@ cost_per_day = 3
 cost_of_rentals = (little_mermaid + brother_bear + hercules) * cost_per_day
 
 
-#  Contractor
+# Weekly Pay for Tech
+hourly_rate_google = 400
+hourly_rate_facebook = 350
+hourly_rate_amazon = 380
 
-employers = [
-    {'company': 'Google', 'pay_rate': 400, 'hrs_this_week': 6}, 
-    {'company': 'Amazon', 'pay_rate': 380, 'hrs_this_week': 4}, 
-    {'company': 'Facebook', 'pay_rate': 350, 'hrs_this_week': 10}
-]
+hours_worked_google = 8
+hours_worked_facebook = 3
+hours_worked_amazon = 6
 
+total_pay_google = hourly_rate_google * hours_worked_google
+total_pay_facebook = hourly_rate_facebook * hours_worked_facebook
+total_pay_amazon = hourly_rate_amazon * hours_worked_amazon
 
+total_pay = total_pay_amazon + total_pay_facebook + total_pay_google
 
+total_pay
+
+# Available for Class
+class_is_full = True
+schedule_conflict = False
+
+student_can_enroll = (not class_is_full) and (not schedule_conflict)
+
+student_can_enroll
+
+#
+offer_expired = True
+premium_member = False
+number_of_items_bought = 3
+items_needed_for_offer = 2
+
+can_be_applied = (not offer_expired) and (premium_member or (number_of_items_bought > items_needed_for_offer))
+
+can_be_applied
